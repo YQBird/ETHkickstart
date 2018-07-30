18ca82e111029686172551b1db33065b91823e33
Yan, Qun <yq1119apzz@gmail.com>
2018-07-30 11:59:02 -0400

    support loading web3 for both client & server side, load inital data in the default component

--- ethereum/factory.js
+++ ethereum/factory.js
@@ -3,7 +3,7 @@ import FactoryContract from "./build/CampaignFactory.json";
 
 const instance = new web3.eth.Contract(
   JSON.parse(FactoryContract.interface),
-  "0x55B873292EDE281a2bC756189131B9EEB1A09CDf"
+  "0x5e16EBe4237FF603195E59D1c43C50b4a4c22CBD"
 );
 
 export default instance;
--- ethereum/web3.js
+++ ethereum/web3.js
@@ -1,6 +1,20 @@
 // use to create web3 instance
 import Web3 from "web3";
 
-const web3 = new Web3(window.web3.currentProvider);
+// need both server&client side web3 instance
+// in server side, there is no window object
+let web3;
+
+if (typeof window !== "undefined" && typeof window.web3 !== "undefined") {
+  // we are running in browser & metamastk is running
+  web3 = new Web3(window.web3.currentProvider);
+} else {
+  // we are running on server or metamask is not running
+  const provider = new Web3.providers.HttpProvider(
+    "https://rinkeby.infura.io/v3/168610cf92e94654b88173cd7cb508f9"
+  );
+
+  web3 = new Web3(provider);
+}
 
 export default web3;
